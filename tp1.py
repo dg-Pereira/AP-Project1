52cc7a178332438855a573cf38afc9a65480af54
dg-Pereira <dg.pereira@campus.fct.unl.pt>
2022-04-27 17:23:27 +0100

    Used adam optimizer and changed network architecture a bit. Now, validation error reaches 80-85%

--- tp1.py
+++ tp1.py
@@ -4,6 +4,7 @@
 Aprendizagem Profunda, TP1
 """
 
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.optimizers import SGD
 from tp1_utils import load_data
@@ -11,9 +12,10 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D
 from tensorflow.keras.layers import Activation, Flatten, Dropout, Dense
 
-INIT_LR=0.001
-NUM_EPOCHS=25
-BATCH_SIZE = 64
+#validation error was jumping a lot, so used lower learning rate
+INIT_LR = 0.001
+NUM_EPOCHS = 250
+BATCH_SIZE = 32
 
 all_data = load_data()
 
@@ -31,34 +33,47 @@ print(train_classes.shape)
 print(test_X.shape)
 print(test_classes.shape)
 
+#data is already normalized
+
+
 def create_model():
-    model = Sequential()
-    model.add(Conv2D(32, (3,3), padding="same", input_shape=(64, 64, 3)))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization())
-    
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    m = Sequential()
+    m.add(Conv2D(32, (3, 3), padding="same", input_shape=(64, 64, 3)))
+    m.add(Activation('relu'))
+    m.add(BatchNormalization())
 
-    model.add(Conv2D(64, (3,3), padding='same'))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization())
-    
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization())
-    
-    model.add(Dropout(0.5))
-    model.add(Dense(10))
-    model.add(Activation("softmax"))
-    return model
-    
-opt = SGD(lr=INIT_LR, momentum=0.9, decay=INIT_LR/NUM_EPOCHS)
+    m.add(MaxPooling2D(pool_size=(2, 2)))
+
+    m.add(Conv2D(64, (3, 3), padding='same'))
+    m.add(Activation('relu'))
+    m.add(BatchNormalization())
+
+    m.add(MaxPooling2D(pool_size=(2, 2)))
+
+    m.add(Flatten())
+    m.add(Dense(512))
+    m.add(Activation('relu'))
+    m.add(BatchNormalization())
+
+    m.add(Dense(256))
+    m.add(Activation('relu'))
+    m.add(BatchNormalization())
+
+    m.add(Dense(128))
+    m.add(Activation('relu'))
+    m.add(BatchNormalization())
+
+    m.add(Dropout(0.5))
+    m.add(Dense(10))
+    m.add(Activation("softmax"))
+    return m
+
+
+opt = tf.keras.optimizers.Adam(lr=INIT_LR)
 model = create_model()
-model.compile(loss="categorical_crossentropy", optimizer = opt, metrics = ["accuracy"])
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-history = model.fit(train_X, train_classes, validation_data=(test_X, test_classes), batch_size=BATCH_SIZE, epochs=NUM_EPOCHS)
+history = model.fit(train_X, train_classes, validation_data=(test_X, test_classes), batch_size=BATCH_SIZE,
+                    epochs=NUM_EPOCHS)
 
 model.summary()
